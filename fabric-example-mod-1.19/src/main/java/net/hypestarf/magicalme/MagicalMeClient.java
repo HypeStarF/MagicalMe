a24775b120b1c21359b1e279c1d4128fbbf41345
HypeStarF <love.norrstrand@gmail.com>
2022-07-18 12:28:00 +0200

    Initial commit

---
+++ fabric-example-mod-1.19/src/main/java/net/hypestarf/magicalme/MagicalMeClient.java
@@ -0,0 +1,10 @@
+package net.hypestarf.magicalme;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicalMeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
